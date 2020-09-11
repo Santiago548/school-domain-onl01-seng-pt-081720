24fb9351f3237138296352893f36909c37a548a2
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 00:35:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,13 @@
 class School
 
   attr_accessor :roster
-
+    
   def initialize(roster)
     @roster = {}
   end
 
   def add_student(name, grade)
-
+    @roster[grade] = []  
+    @roster[grade] << "#{name}"
   end
 end
